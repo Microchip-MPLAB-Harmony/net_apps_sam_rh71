cad99526ffbe7304c18f453f3b91ff4c25978a2e
Joffrey COFFINEAU <joffrey.coffineau@microchip.com>
2022-10-21 12:05:03 +0200

    DFT Regeneration and test for tcpip_udp_client_server_sam_rh71_ek MH3-60538

--- apps/tcpip_udp_client_server/firmware/src/app.h
+++ apps/tcpip_udp_client_server/firmware/src/app.h
@@ -57,6 +57,14 @@
 #include "system_config.h"
 #include "system_definitions.h"
 
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Type Definitions
@@ -64,7 +72,7 @@
 // *****************************************************************************
 
 // *****************************************************************************
-/* Application States
+/* Application states
 
   Summary:
     Application states enumeration
@@ -123,7 +131,6 @@ typedef struct
     uint64_t    mTimeOut;
 } APP_DATA;
 
-
 // *****************************************************************************
 // *****************************************************************************
 // Section: Application Callback Routines
@@ -132,7 +139,6 @@ typedef struct
 /* These routines are called by drivers when certain events occur.
 */
 
-	
 // *****************************************************************************
 // *****************************************************************************
 // Section: Application Initialization and State Machine Functions
@@ -203,12 +209,17 @@ void APP_Initialize ( void );
     This routine must be called from SYS_Tasks() routine.
  */
 
-void APP_Tasks ( void );
+void APP_Tasks( void );
 
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
 
 #endif /* _APP_H */
+
 /*******************************************************************************
  End of File
  */
-
 
