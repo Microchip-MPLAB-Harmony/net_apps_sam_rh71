a21aea1084f4732e45d746618fad9cc4464262ba
Joffrey COFFINEAU <joffrey.coffineau@microchip.com>
2022-10-21 11:55:19 +0200

    DFT Regeneration and test for tcpip_udp_client_sam_rh71_ek MH3-60536

--- apps/tcpip_udp_client/firmware/src/app.h
+++ apps/tcpip_udp_client/firmware/src/app.h
@@ -57,6 +57,14 @@
 #include "system_config.h"
 #include "system_definitions.h"
 
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Type Definitions
@@ -64,7 +72,7 @@
 // *****************************************************************************
 
 // *****************************************************************************
-/* Application States
+/* Application states
 
   Summary:
     Application states enumeration
@@ -117,7 +125,6 @@ typedef struct
 
 } APP_DATA;
 
-
 // *****************************************************************************
 // *****************************************************************************
 // Section: Application Callback Routines
@@ -126,7 +133,6 @@ typedef struct
 /* These routines are called by drivers when certain events occur.
 */
 
-	
 // *****************************************************************************
 // *****************************************************************************
 // Section: Application Initialization and State Machine Functions
@@ -197,10 +203,16 @@ void APP_Initialize ( void );
     This routine must be called from SYS_Tasks() routine.
  */
 
-void APP_Tasks ( void );
+void APP_Tasks( void );
 
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
 
 #endif /* _APP_H */
+
 /*******************************************************************************
  End of File
  */
